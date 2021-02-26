ab46202c7577a56acf85a18aa8f127d41881e0d5
ZsofiaFarago <farago.zsofia.1995@gmail.com>
2021-02-27 00:54:39 +0100

    Lábléc html kialakítása

--- Project/protected/footer.php
+++ Project/protected/footer.php
@@ -1,5 +1,42 @@
-		<div id="footer">
-			<p>Copyright &copy; 2020 AFP1 Trio. All Rights Reserved</p>
-		</div>
+		<footer class="footer-distributed">
+			<div class="footer-left">
+				<h3>Eagle Flies</h3>
+				<p class="footer-links">
+					<a href="#">Home</a> | 
+					<a href="#">Blog</a> | 
+					<a href="#">Pricing</a> | 
+					<a href="#">About</a> | 
+					<a href="#">Faq</a> | 
+					<a href="#">Contact</a>
+				</p>
+				<p class="footer-company-name">Trio &copy; 2021</p>
+			</div>
+			<div class="footer-center">
+				<div>
+					<i class="fas fa-map-marker-alt"></i>
+					<p><span>Leányka u. 4.</span>3300, Eger</p>
+				</div>
+				<div>
+					<i class="fas fa-phone"></i>
+					<p>+1 555 123456</p>
+				</div>
+				<div>
+					<i class="fas fa-envelope"></i>
+					<p><a href="mailto:contact@trio.com">contact@trio.com</a></p>
+				</div>
+			</div>
+			<div class="footer-right">
+				<p class="footer-company-about">
+					<span>A vállalkozásról</span>
+					Startup vállalkozások számára nyújtunk lehetőséget saját honlappal megjelenni. Vállaljuk ennek elkészítését és oldalunkon való megjelenítését.
+				</p>
+				<div class="footer-icons">
+					<a href="#"><i class="fab fa-facebook-f"></i></a>
+					<a href="#"><i class="fab fa-twitter"></i></a>
+					<a href="#"><i class="fab fa-linkedin"></i></a>
+					<a href="#"><i class="fab fa-github"></i></a>
+				</div>
+			</div>
+		</footer>
 	</body>
 </html>
