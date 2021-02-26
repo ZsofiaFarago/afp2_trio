ec2dd1931dd30e487cc62c144d8f87e03c6ef9a5
ZsofiaFarago <farago.zsofia.1995@gmail.com>
2021-02-26 22:52:33 +0100

    A főoldal fejlécének és menüjének kialaktása

--- Project/protected/header.php
+++ Project/protected/header.php
@@ -2,10 +2,14 @@
 <html>
 	<head>
 		<meta content="charset=UTF-8">
-		<title></title>
+		<title>Eagle Flies - startup vállalkozások oldalai</title>
+		<link rel="stylesheet" href="<?=PUBLIC_DIR.'css/main_style.css'?>">
+		<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 	</head>
 	<body>		
 		<div id="header">
+			<img id="logo" src="<?=PUBLIC_DIR.'images/main/logo.png'?>" />
+			<h1 id="title">A startup vállalkozások gyűjtőhelye</h1>
 		</div>
 		<div id="nav">
 			<div id="menu">
