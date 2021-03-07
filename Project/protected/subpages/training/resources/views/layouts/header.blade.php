f2f97634a1c08a39305051240d337e0acbc3a081
Spokieh <dohanyos96@gmail.com>
2021-03-07 18:56:03 +0100

    Alap navbar letrehozva

--- Project/protected/subpages/training/resources/views/layouts/header.blade.php
+++ Project/protected/subpages/training/resources/views/layouts/header.blade.php
@@ -1,1 +1,21 @@
-<h3>Header</h3>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Fooldal</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Rendeles</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Kosar(0)</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
