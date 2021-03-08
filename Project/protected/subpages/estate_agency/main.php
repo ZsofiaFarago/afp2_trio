1f73a78c5c2d7b2c46b26b8cdb6e5cbe5f1e2f6d
ZsofiaFarago <farago.zsofia.1995@gmail.com>
2021-03-09 00:13:14 +0100

    Ingatlanos oldal: főoldal tartalma I.

--- Project/protected/subpages/estate_agency/main.php
+++ Project/protected/subpages/estate_agency/main.php
@@ -1,1 +1,53 @@
-A ingatlanos oldal főoldala
+<div id="main">
+	<div id="offer">
+		<div class="div_content">
+			<h2>Kedvező ajánlat</h2>
+			<p>Vegye igénybe egyszerre két szolgáltatásunkat és kérjen ingatlan-értékbecslést energetikai tanúsítvánnyal! Ekkor 20% kedvezményt adunk a teljes árból.</p>
+			<a href=""><div class="go_on">Érdekel</div></a>
+		</div>
+	</div>
+
+	<div id="calculate">
+		<div class="div_content">
+			<h2>Számítsa ki kiadásait!</h2>
+			<p>Ingatlant szeretne eladni vagy vásárolni, és érdeklik a felmerülő költségek! Kalkulátoraink segítségével számtsa ki az Önt érintő személyi jövedelemadó vagy vagyonszerzési illeték értékét!</p>
+			<a href=""><div class="go_on">Érdekel</div></a>
+		</div>
+
+	</div>
+
+	<div id="experts">
+		<div class="div_content">
+			<h2>Kérje szakértőink segítségét!</h2>
+			<p>Ingatlan-ügynökeink elérhetőségei nyilvánosak, kérje bátran segítségüket!</p>
+			<a href=""><div class="go_on">Érdekel</div></a>
+			<div id="flex-container">
+				<div id="flex-child">
+		    		<img src="<?=PUBLIC_DIR.'/images/estate_agency/agent_001.jpg'?>" />
+		    		<p class="expert_name">Gordon Ramsay</p>
+		  		</div>
+		  		<div id="flex-child">
+		    		<img src="<?=PUBLIC_DIR.'/images/estate_agency/agent_002.jpg'?>" />
+		    		<p class="expert_name">Cersei Lannister</p>
+		  		</div>
+		  		<div id="flex-child">
+		    		<img src="<?=PUBLIC_DIR.'/images/estate_agency/agent_003.png'?>" />
+		    		<p class="expert_name">Anakin Skywalker</p>
+		  		</div>
+		  		<div id="flex-child">
+		    		<img src="<?=PUBLIC_DIR.'/images/estate_agency/agent_004.jpg'?>" />
+		    		<p class="expert_name">Perselus Piton</p>
+		  		</div>
+			</div>
+		</div>
+
+	</div>
+
+	<div id="contact_us">
+		<div class="div_content">
+			<h2>Lépjen velünk kapcsolatba!</h2>
+			<p>Kapcsolati űrlapunk segítségével neve, email címe megadásával üzenetet küldhet nekünk, melyben felteheti kérdéseit ingatlanos ügyeivel kapcsolatban, és szakértőink igyekeznek azt rövid időn belül megválaszolni az Ön által megadott email címre küldött üzenetben.</p>
+			<a href=""><div class="go_on">Érdekel</div></a>
+		</div>
+	</div>
+</div>
