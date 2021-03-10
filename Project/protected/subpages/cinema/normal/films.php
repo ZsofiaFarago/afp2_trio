7c309898e280ccaa3e03ef4fbab5854ca2942e3c
Marci <pmarcell50@gmail.com>
2021-03-10 22:37:10 +0100

    Mozi oldal: filmek táblázatának létrehozása.. PHP ciklusra vár

--- Project/protected/subpages/cinema/normal/films.php
+++ Project/protected/subpages/cinema/normal/films.php
@@ -1,3 +1,20 @@
 <h2><center>Filmek listája</center></h2>
 <hr>
 <p>FEJLESZTÉS ALATT...</p>
+
+<div class="item">
+	<table style="table-layout: fixed; width: 60%; border: 3px solid gray; margin-left: auto; margin-right: auto;   background-color: #bababa;">
+		<tr>
+			<td colspan="2"><center>Film neve</center></td>			
+		</tr>
+		<tr>
+			<td><b>Elérhető időpontok:</b></td>
+			<td style="text-align: center"></td>			
+		</tr>
+		<tr>
+			<td>Leírás..</td>
+		<tr>
+		<td colspan="2">Ár: ... <center>										
+			</tr>
+	</table>
+</div>
