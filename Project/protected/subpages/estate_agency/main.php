fe2b2e848e6c68e8992f64d8a7c686f95f406e51
ZsofiaFarago <farago.zsofia.1995@gmail.com>
2021-03-10 01:18:22 +0100

    Ingaltlanos oldal főoldalához slideshow és contacts

--- Project/protected/subpages/estate_agency/main.php
+++ Project/protected/subpages/estate_agency/main.php
@@ -1,4 +1,51 @@
 <div id="main">
+	<div id="catalogue">
+		<div class="slideshow-container">
+			<div class="mySlides fade">
+				<img src="<?=PUBLIC_DIR.'/images/estate_agency/estate_001.jpg'?>">
+			</div>
+			<div class="mySlides fade">
+				<img src="<?=PUBLIC_DIR.'/images/estate_agency/estate_002.jpg'?>">
+			</div>
+			<div class="mySlides fade">
+				<img src="<?=PUBLIC_DIR.'/images/estate_agency/estate_003.jpg'?>">
+			</div>
+			<div class="mySlides fade">
+				<img src="<?=PUBLIC_DIR.'/images/estate_agency/estate_004.jpg'?>">
+			</div>
+		</div>
+		<br/>
+		<div id="dots">
+			<span class="dot"></span>
+			<span class="dot"></span>
+			<span class="dot"></span>
+			<span class="dot"></span>
+		</div>
+	</div>
+	
+	<script>
+		var slideIndex = 1;
+		showSlides();
+
+		function showSlides() {
+			var i;
+			var slides = document.getElementsByClassName("mySlides");
+			var dots = document.getElementsByClassName("dot");
+			
+			for (i = 0; i < slides.length; i++) {
+			    slides[i].style.display = "none";  
+			}
+			slideIndex++;
+			if (slideIndex > slides.length) {slideIndex = 1} 
+			for (i = 0; i < dots.length; i++) {
+			    dots[i].className = dots[i].className.replace(" active", "");
+			}
+			slides[slideIndex-1].style.display = "block";  
+			dots[slideIndex-1].className += " active";
+			setTimeout(showSlides, 2000);
+		}
+	</script>
+
 	<div id="offer">
 		<div class="div_content">
 			<h2>Kedvező ajánlat</h2>
@@ -48,6 +95,46 @@
 			<h2>Lépjen velünk kapcsolatba!</h2>
 			<p>Kapcsolati űrlapunk segítségével neve, email címe megadásával üzenetet küldhet nekünk, melyben felteheti kérdéseit ingatlanos ügyeivel kapcsolatban, és szakértőink igyekeznek azt rövid időn belül megválaszolni az Ön által megadott email címre küldött üzenetben.</p>
 			<a href=""><div class="go_on">Érdekel</div></a>
+		</div>
+	</div>
+	<div id="contact_details">
+		<div class="column" id="details_left">
+			<div>
+				<h3>Estate Agency</h3>
+				<p id="links">
+					<a href="#">Home</a> |
+					<a href="#">Blog</a> |
+					<a href="#">Pricing</a> |
+					<a href="#">About</a> |
+					<a href="#">Faq</a> |
+					<a href="#">Contact</a>
+				</p>
+				<p id="company_name">Estate Agency &copy; 2021</p>
+			</div>
+		</div>
+
+		<div class="column" id="details_center">
+			<div>
+				<div>
+					<i class="fas fa-map-marker-alt"></i>
+					<p><span>Leányka u. 4.</span>3300, Eger</p>
+				</div>
+				<div>
+					<i class="fas fa-phone"></i>
+					<p>+1 555 123456</p>
+				</div>
+				<div>
+					<i class="fas fa-envelope"></i>
+					<a href="mailto:contact@trio.com">contact@trio.com</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="column" id="details_right">
+			<div>
+				<h3>A vállalkozásról</h3>
+				<p>Startup vállalkozások számára nyújtunk lehetőséget saját honlappal megjelenni. Vállaljuk ennek elkészítését és oldalunkon való megjelenítését.</p>
+			</div>
 		</div>
 	</div>
 </div>
