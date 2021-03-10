cff46ba8d3ffecafd32ea16a243c8e620af1067d
Marci <pmarcell50@gmail.com>
2021-03-10 22:02:30 +0100

    Mozi oldal: adatbázis konfigurációs fájljának hozzáadása

---
+++ Project/protected/subpages/cinema/db_config.php
@@ -0,0 +1,13 @@
+<?php 
+define('CN_PROTECTED_DIR', BASE_DIR.'protected/subpages/cinema/');
+
+define('DATABASE_CONTROLLER', CR_PROTECTED_DIR.'database.php');
+define('USER_MANAGER', CR_PROTECTED_DIR.'userManager.php');
+
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'cinema');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_CHARSET', 'utf8');
+?>
