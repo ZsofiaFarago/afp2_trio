d42753d8a8889d5b11dc64931d2e58b22a0d7455
Ravasz Bálint <ravasz.balint7@gmail.com>
2021-03-11 10:31:07 +0100

    Állásportál oldal: Primary key ID beállítása

--- Project/protected/subpages/job/sql/categories.sql
+++ Project/protected/subpages/job/sql/categories.sql
@@ -45,3 +45,23 @@ INSERT INTO `categories` (`id`, `name`) VALUES
 --
 -- Indexes for dumped tables
 --
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
