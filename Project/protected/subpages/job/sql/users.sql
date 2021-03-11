2a007e25d2d3ae00f19baf25a80aba4952edc0a2
Ravasz Bálint <ravasz.balint7@gmail.com>
2021-03-11 10:37:40 +0100

    Állásportál oldal: users tábla feltöltése tesztadatokkal

--- Project/protected/subpages/job/sql/users.sql
+++ Project/protected/subpages/job/sql/users.sql
@@ -35,3 +35,17 @@ CREATE TABLE `users` (
   `fname` varchar(100) NOT NULL,
   `permission` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `lname`, `fname`, `permission`) VALUES
+(3, 'annaba', '123123', '', '', 0),
+(6, 'asd@asd.com', '555555', '', '', 0),
+(7, 'ravasz.balint7@gmail.com', 'Admin1', 'Ravasz', 'Bálint', 1),
+(10, 'ravasz.balint7@gmail.com', '7c222fb2927d828af22f592134e8932480637c0d', 'Ravasz', 'Bálint', 0),
+(11, 'ravasz.balint7@gmail.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'Ravasz', 'Bálint', 0),
+(12, 'ravasz.balint@gmail.com', '123456', 'Ravasz', 'Bálint', 0),
+(13, 'asd@asd.com', '123456', 'asd', 'asd', 0),
+(14, 'asd2@asd.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'Bálint', 'asd', 0);
