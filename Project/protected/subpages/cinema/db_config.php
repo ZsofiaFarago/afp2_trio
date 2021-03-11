9870088af1083aef3ed8f1f22f3fb3bf134c6c4f
Marci <pmarcell50@gmail.com>
2021-03-11 12:30:44 +0100

    Mozi oldal: CSS fájl hozzárendelése+config hotfix

--- Project/protected/subpages/cinema/db_config.php
+++ Project/protected/subpages/cinema/db_config.php
@@ -4,8 +4,8 @@ define('BASE_DIR', './');
 define('PUBLIC_DIR', BASE_DIR.'public/');
 define('CN_PROTECTED_DIR', BASE_DIR.'protected/subpages/cinema/');
 
-define('DATABASE_CONTROLLER', CR_PROTECTED_DIR.'database.php');
-define('USER_MANAGER', CR_PROTECTED_DIR.'userManager.php');
+define('DATABASE_CONTROLLER', CN_PROTECTED_DIR.'database.php');
+define('USER_MANAGER', CN_PROTECTED_DIR.'userManager.php');
 
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
