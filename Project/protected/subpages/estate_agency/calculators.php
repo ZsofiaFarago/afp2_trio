5a62856d863a389f02db12d2ca0a050f59820f1c
ZsofiaFarago <farago.zsofia.1995@gmail.com>
2021-03-12 17:05:08 +0100

    Ingatlanos oldal: személyi jövedelemadó űrlap

--- Project/protected/subpages/estate_agency/calculators.php
+++ Project/protected/subpages/estate_agency/calculators.php
@@ -1,1 +1,16 @@
-Második menüpont tartalma...
+<div id="calculators_page">
+	<div class="calculatorForm" id="incomeTaxCalculator">
+		<h2>Személyi jövedelemadó kalkulátor</h2>
+		<p>Adja meg az alábbi adatokat, és mi megmondjuk, pontosan mekkora összegű személyi jövedelemadóra számíthat ingatlaneladás esetén!</p>
+		<form action="#">
+	    	<label for="acquisitionYear">Az ingatlan megszerzésének éve: <em>&#x2a;</em></label>
+			<input id="acquisitionYear" name="acquisitionYear" required="" type="text" />
+			<label for="acquisitionPrice">Az ingatlan megszerzésének értéke: <em>&#x2a;</em></label>
+			<input id="acquisitionPrice" name="acquisitionPrice" required="" type="email" />
+			<label for="plannedSellingPrice">Tervezett eladási ár: <em>&#x2a;</em></label>
+			<input id="plannedSellingPrice" name="plannedSellingPrice" required="" type="text" />
+			<h3>A *-gal jelölt mezők kitöltése kötelező.</h3>
+			<button id="customerOrder" type="submit">Számítás</button>
+		</form>
+	</div>
+</div>
