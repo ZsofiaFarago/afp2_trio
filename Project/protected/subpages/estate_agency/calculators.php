8c97b555cf0cb5b557a5fc54364bba3f89386161
ZsofiaFarago <farago.zsofia.1995@gmail.com>
2021-03-13 15:22:26 +0100

    Ingatlanos oldal: kalkulátor űrlapok javítása

--- Project/protected/subpages/estate_agency/calculators.php
+++ Project/protected/subpages/estate_agency/calculators.php
@@ -2,34 +2,50 @@
 	<div class="calculatorForm" id="incomeTaxCalculator">
 		<h2>Személyi jövedelemadó kalkulátor</h2>
 		<p>Adja meg az alábbi adatokat, és mi megmondjuk, pontosan mekkora összegű személyi jövedelemadóra számíthat ingatlaneladás esetén!</p>
-		<form action="#">
-	    	<label for="acquisitionYear">Az ingatlan megszerzésének éve: <em>&#x2a;</em></label>
+		<form method="POST">
+	    		<label for="acquisitionYear">Az ingatlan megszerzésének éve: <em>&#x2a;</em></label>
 			<input id="acquisitionYear" name="acquisitionYear" required="" type="text" />
+
 			<label for="acquisitionPrice">Az ingatlan megszerzésének értéke: <em>&#x2a;</em></label>
-			<input id="acquisitionPrice" name="acquisitionPrice" required="" type="email" />
+			<input id="acquisitionPrice" name="acquisitionPrice" required="" type="text" />
+
 			<label for="plannedSellingPrice">Tervezett eladási ár: <em>&#x2a;</em></label>
 			<input id="plannedSellingPrice" name="plannedSellingPrice" required="" type="text" />
+
 			<h3>A *-gal jelölt mezők kitöltése kötelező.</h3>
-			<button id="customerOrder" type="submit">Számítás</button>
+			<button name="incomeTax" type="submit">Számítás</button>
 		</form>
 	</div>
 
 	<div class="calculatorForm" id="acquisitionTaxCalculator">
-		<h2>Személyi jövedelemadó kalkulátor</h2>
+		<h2>Vagyonszerzési illeték kalkulátor</h2>
 		<p>Adja meg az alábbi adatokat, és mi megmondjuk, pontosan mekkora összegű vagyonszerzési illetékre számíthat ingatlanvásárlás esetén!</p>
 		<form action="#">
 			<label for="acquisitionPrice">Az ingatlan megszerzésének értéke: <em>&#x2a;</em></label>
-			<input id="acquisitionPrice" name="acquisitionPrice" required="" type="email" />
+			<input id="acquisitionPrice" name="acquisitionPrice" required="" type="text" />
+
 			<label for="sellWithinOneYear">Adott el ingatlant egy éven belül? </label>
  			<input type="checkbox" id="sellWithinOneYear" name="sellWithinOneYear" value="sellWithinOneYear">
+ 			<label for="sellingPrice">Ebben az esetben adja meg az eladott ingatlan értékét!</label>
+			<input id="sellingPrice" name="sellingPrice" type="text" />
+
 			<label for="forRelatives">Átruházás házastársak, egyenesági rokonok között? </label>
 			<input type="checkbox" id="forRelatives" name="forRelatives" value="forRelatives">
-			<label for="firstProperty">Első lakástulajdon? </label>
-			<input type="checkbox" id="firstProperty" name="firstProperty" value="Boat">
+
+			<label for="newEstate">Új építésű ingatlant vásárol? </label>
+			<input type="checkbox" id="newEstate" name="newEstate" value="newEstate">
+
+			<label for="firstProperty">Első lakástulajdon és Ön 35 év alatti? </label>
+			<input type="checkbox" id="firstProperty" name="firstProperty" value="firstProperty">
+
 			<label for="selfGoverning">Önkormányzati lakás? </label>
 			<input type="checkbox" id="selfGoverning" name="selfGoverning" value="selfGoverning">
+
+			<label for="plot">Telket vásárol és 4 éven belül lakóházat épít rajta? </label>
+			<input type="checkbox" id="plot" name="plot" value="plot">
+
 			<h3>A *-gal jelölt mezők kitöltése kötelező.</h3>
-			<button id="customerOrder" type="submit">Számítás</button>
+			<button name="acquisitionTax" type="submit">Számítás</button>
 		</form>
 	</div>
 </div>
