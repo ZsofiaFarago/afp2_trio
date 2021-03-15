7e57692b25fd6223e4ec20418ddc89cb54c33da0
Spokieh <dohanyos96@gmail.com>
2021-03-15 21:38:12 +0100

    Training - termekek keresesehez route es funkc.

--- Project/protected/subpages/training/app/Http/Controllers/ProductController.php
+++ Project/protected/subpages/training/app/Http/Controllers/ProductController.php
@@ -18,4 +18,9 @@ class ProductController extends Controller
 
         return view('detail', ['product' => $data]);
     }
+    function search(Request $request)
+    {
+        $data = Product::where('name', 'like', '%' . $request->input('query') . '%')->get();
+        return view('search', ['products' => $data]);
+    }
 }
