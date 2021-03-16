ae7bf8486e8a177885904a3556c6d3bbf8ce60dd
Spokieh <dohanyos96@gmail.com>
2021-03-16 17:29:15 +0100

    Training - adatbazisok kapcsolasa, kosar tartalmanak kilistazhatosaga

--- Project/protected/subpages/training/app/Http/Controllers/ProductController.php
+++ Project/protected/subpages/training/app/Http/Controllers/ProductController.php
@@ -7,6 +7,7 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use League\CommonMark\Inline\Parser\EscapableParser;
 use Session;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
@@ -42,5 +43,16 @@ class ProductController extends Controller
     {
         $userId = Session::get('user')['id'];
         return Cart::where('user_id', $userId)->count();
+    }
+    function cartList()
+    {
+        $userId = Session::get('user')['id'];
+        $products = DB::table('cart')
+            ->join('products', 'cart.product_id', '=', 'products.id')
+            ->where('cart.user_id', $userId)
+            ->select('products.*', 'cart.id as cart_id')
+            ->get();
+
+        return view('cart', ['products' => $products]);
     }
 }
