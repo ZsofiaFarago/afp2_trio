79f12c6bc78e6324ea46efb4c14d085308d55e9b
Ravasz Bálint <ravasz.balint7@gmail.com>
2021-03-17 17:17:26 +0100

    Állásportál oldal: Kapcsolódás az adatbázishoz

---
+++ Project/protected/subpages/job/database.php
@@ -0,0 +1,16 @@
+<?php 
+function getConnection() {
+	$connection = new PDO(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME.';',DB_USER, DB_PASS);
+	$connection->exec("SET NAMES '".DB_CHARSET."'");
+	return $connection;
+}
+
+function getRecord($queryString, $queryParams = []) {
+	$connection = getConnection();
+	$statement = $connection->prepare($queryString);
+	$success = $statement->execute($queryParams);
+	$result = $success ? $statement->fetch(PDO::FETCH_ASSOC) : [];
+	$statement->closeCursor();
+	$connection = null;
+	return $result;
+}
