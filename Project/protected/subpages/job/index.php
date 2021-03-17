5d75844d61b1c9190c178d8e175f4b2d21aa8efe
Ravasz Bálint <ravasz.balint7@gmail.com>
2021-03-17 12:55:27 +0100

    Állásportál oldal: index.php hozzáadása

---
+++ Project/protected/subpages/job/index.php
@@ -0,0 +1,18 @@
+
+<?php session_start(); ?>
+<?php include_once 'config/init.php'; ?>
+<?php include_once 'config/config.php'; ?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Login</title>
+		<link rel="stylesheet" href="css/style.css">
+		<?php include_once 'nav.php'; ?>
+	</head>
+	<body>
+		<?php include_once 'routing.php'; ?>
+	</body>
+</html>
