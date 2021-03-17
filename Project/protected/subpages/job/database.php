8056bf3d01f0a8a2b51a16a1ac0b170336f17864
Ravasz Bálint <ravasz.balint7@gmail.com>
2021-03-17 17:18:21 +0100

    Állásportál oldal: Végleges adatbázis kapcsolat

--- Project/protected/subpages/job/database.php
+++ Project/protected/subpages/job/database.php
@@ -14,3 +14,33 @@ function getRecord($queryString, $queryParams = []) {
 	$connection = null;
 	return $result;
 }
+
+function getList($queryString, $queryParams = []) {
+	$connection = getConnection();
+	$statement = $connection->prepare($queryString);
+	$success = $statement->execute($queryParams);
+	$result = $success ? $statement->fetchAll(PDO::FETCH_ASSOC) : [];
+	$statement->closeCursor();
+	$connection = null;
+	return $result;
+}
+
+function executeDML($queryString, $queryParams = []) {
+	$connection = getConnection();
+	$statement = $connection->prepare($queryString);
+	$success = $statement->execute($queryParams);
+	$statement->closeCursor();
+	$connection = null;
+	return $success;
+}
+
+function getField($queryString, $queryParams = []) {
+	$connection = getConnection();
+	$statement = $connection->prepare($queryString);
+	$success = $statement->execute($queryParams);
+	$result = $success ? $statement->fetch()[0]: [];
+	$statement->closeCursor();
+	$connection = null;
+	return $result;
+}
+?>
