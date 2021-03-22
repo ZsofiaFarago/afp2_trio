3ad107f4cb7c3b4aaedca076af8c07282bc56d41
ZsofiaFarago <farago.zsofia.1995@gmail.com>
2021-03-22 10:54:49 +0100

    Ingatlanügynökök menüpont: elrendezés, szín + reszponzivitás

--- Project/protected/subpages/estate_agency/agents.php
+++ Project/protected/subpages/estate_agency/agents.php
@@ -1,4 +1,8 @@
 <div id="agents-div">
+	<div id="experts-main-div">
+		<h1>Kérje szakértőink segítségét!</h1>
+		<p>Csapattagjaink mind profi ingatlanügynökök, akik a szakma különböző területeinek a szakértői. Bátran keressék őket emailben, telefonon vagy akár személyesen is minden hétköznap 9 és 15 óra között! Ők azok, akik további tudnivalókkal rendelkeznek szolgáltatásainkról, és az oldalunkon közölt legfontosabb tudnivalókon kívül bővebb információval állnak rendelkezésre.</p>
+	</div>
 	<?php
 		include_once PROTECTED_DIR.'subpages/estate_agency/core/MyController.php';
 		include_once PROTECTED_DIR.'subpages/estate_agency/controller/agentController.php';
