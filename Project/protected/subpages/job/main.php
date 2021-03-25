4cf899c2c49550212879491f28a7a80f2b3fe07a
Ravasz Bálint <ravasz.balint7@gmail.com>
2021-03-25 10:17:10 +0100

    Állásportál oldal: Login inicializálás

--- Project/protected/subpages/job/main.php
+++ Project/protected/subpages/job/main.php
@@ -1,1 +1,16 @@
-Az álláskeresés oldal főoldala
+<?php if(!loggedIn()) : ?>
+	<div class="login">
+		<h1>Login</h1>
+		<form method="post">
+			<label for="Email">
+				<i class="fas fa-user"></i>
+			</label>
+			<input type="text" name="email" placeholder="Email" id="email" required>
+			<label for="password">
+				<i class="fas fa-lock"></i>
+			</label>
+			<input type="password" name="password" placeholder="Password" id="password" required>
+			<input type="submit" value="Login" name="submitlogin" id="submitlogin">
+		</form>
+	</div>
+<?php endif; ?>
