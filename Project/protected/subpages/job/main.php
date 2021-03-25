04a568be3e4a5dbe566aa1e7c5923eff27088174
Ravasz Bálint <ravasz.balint7@gmail.com>
2021-03-25 10:17:54 +0100

    Állásportál oldal: Login esetek kezelése

--- Project/protected/subpages/job/main.php
+++ Project/protected/subpages/job/main.php
@@ -14,3 +14,30 @@
 		</form>
 	</div>
 <?php endif; ?>
+<?php 
+    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submitlogin'])){
+        $post_data = [
+            'email' => $_POST['email'],
+            'password' => $_POST['password']
+        ];
+     
+		$query = "SELECT id, email, password, permission FROM users WHERE email = :email AND password = :password";
+		$params = [
+            ':email' => $post_data['email'],
+            ':password' => sha1($post_data['password'])
+		];
+		
+		//include_once 'config/config.php';
+        include_once 'database.php';
+		$record = getRecord($query, $params);
+
+		if(empty($record)){
+			echo "Sikertelen bejelentkezés";
+		}
+		else{
+			$_SESSION['permission'] = $record['permission'];
+			echo "Permission: ".$_SESSION['permission'];
+		}
+		header('Location: index.php');
+    }
+?>
