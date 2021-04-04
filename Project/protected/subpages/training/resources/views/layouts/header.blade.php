a3535fab81d40b9614844096df89c9d462584449
Spokieh <dohanyos96@gmail.com>
2021-04-04 21:25:28 +0200

    Training: navbar menupontok alakitasa

--- Project/protected/subpages/training/resources/views/layouts/header.blade.php
+++ Project/protected/subpages/training/resources/views/layouts/header.blade.php
@@ -21,9 +21,9 @@ if(Session::has('user'))
 		</form>
 		<div class="navbar-nav ml-auto">
 			<a href="/" class="nav-item nav-link active"><i class="fa fa-home"></i><span>Főoldal</span></a>
+      @if(Session::has('user'))
 			<a href="/myorders" class="nav-item nav-link"><i class="fa fa-calendar-check-o"></i><span>Rendelés</span></a>
 			<a href="/cart" class="nav-item nav-link"><i class="fa fa-shopping-cart"></i><span>Kosár ({{ $total }})</span></a>
-      @if(Session::has('user'))
       <div class="nav-item dropdown ">
 				<a href="#" data-toggle="dropdown" class="nav-item nav-link dropdown-toggle user-action"><i class="fa fa-user" aria-hidden="true"></i> {{ Session::get('user')['name'] }}<b class="caret"></b></a>
 				<div class="dropdown-menu">
