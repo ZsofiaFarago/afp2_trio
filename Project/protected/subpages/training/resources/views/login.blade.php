8f20211d42206e979c9949ae9404a2f004d3aeca
Spokieh <dohanyos96@gmail.com>
2021-04-05 21:49:18 +0200

    Training: bejelentezesi form stilus

--- Project/protected/subpages/training/resources/views/login.blade.php
+++ Project/protected/subpages/training/resources/views/login.blade.php
@@ -1,22 +1,32 @@
 @extends('layouts.master')
 @section('content')
-<div class="container" style="height: 500px; padding-top: 100px" >
-    <div class="row">
-        <div class="col-md-4">
-            <form action="/login" method="POSt">
-                <div class="mb-3">
-                  @csrf
-                  <label for="exampleInputEmail1" class="form-label">Email cim</label>
-                  <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                  <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div class="mb-3">
-                  <label for="exampleInputPassword1" class="form-label">Jelszo</label>
-                  <input type="password" name="password" class="form-control" id="exampleInputPassword1">
-                </div>
-                <button type="submit" class="btn btn-primary">Bejelentkezes</button>
-              </form>
-        </div>
-    </div>
+<div class="container">
+
+  <div class="title">
+    <h1>Bejelentkezés</h1>
+  </div>
+
+  <div class="form-groups">
+    <form  action="/login" method="POST">
+      @csrf
+      <div class="group">      
+        <input type="email" name="email" required>
+        <span class="highlight"></span>
+        <span class="bar"></span>
+        <label>E-mail</label>
+      </div>
+      
+      <div class="group">      
+        <input type="password"name="password" required>
+        <span class="highlight"></span>
+        <span class="bar"></span>
+        <label>Jelszó</label>
+      </div>
+  
+      <div class="group">
+        <button type="submit" class="btn btn-primary">Bejelentkezés</button>
+      </div>
+    </form>
+  </div>
 </div>
 @endsection
