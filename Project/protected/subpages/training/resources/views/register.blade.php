85769f8edaaca8b91a559d585b38c8da279c24a7
Spokieh <dohanyos96@gmail.com>
2021-04-05 22:30:09 +0200

    Training: regisztarcios form dizajn

--- Project/protected/subpages/training/resources/views/register.blade.php
+++ Project/protected/subpages/training/resources/views/register.blade.php
@@ -1,27 +1,39 @@
 @extends('layouts.master')
 @section('content')
-<div class="container" style="height: 500px; padding-top: 100px" >
-    <div class="row">
-        <div class="col-md-4">
-            <form action="/register" method="POSt">
-                @csrf
-                <div class="mb-3">
-                  <label for="exampleInputEmail1" class="form-label">Felhasznalo nev</label>
-                  <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="Felhasznalonev">
+<div class="container" >
 
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email cim</label>
-                    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  <div class="title">
+    <h1>Regisztrálás</h1>
+  </div>
 
-                  </div>
-                <div class="mb-3">
-                  <label for="exampleInputPassword1" class="form-label">Jelszo</label>
-                  <input type="password" name="password" class="form-control" id="exampleInputPassword1">
-                </div>
-                <button type="submit" class="btn btn-primary">Regisztralas</button>
-              </form>
-        </div>
-    </div>
+  <div class="form-groups">
+    <form  action="/register" method="POST">
+      @csrf
+      <div class="group">      
+        <input type="text" name="name" id='name' required>
+        <span class="highlight"></span>
+        <span class="bar"></span>
+        <label>Felhasználónév</label>       
+      </div>
+
+      <div class="group">      
+        <input type="email" name="email" required>
+        <span class="highlight"></span>
+        <span class="bar"></span>
+        <label>Email cím</label>       
+      </div>
+      
+      <div class="group">      
+        <input type="password"name="password" required>
+        <span class="highlight"></span>
+        <span class="bar"></span>
+        <label>Jelszó</label>
+      </div>
+  
+      <div class="group">
+        <button type="submit" class="btn btn-primary">Regisztrálás</button>
+      </div>
+    </form>
+</div>
 </div>
 @endsection
