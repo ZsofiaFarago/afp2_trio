0d873fd10561d8d08784b717f21b67738916c456
ZsofiaFarago <farago.zsofia.1995@gmail.com>
2021-04-07 23:33:37 +0200

    Ingatlanos oldal: contacts űrlap input mezőinek validálása

--- Project/protected/subpages/estate_agency/controller/contactController.php
+++ Project/protected/subpages/estate_agency/controller/contactController.php
@@ -1,11 +1,15 @@
 <?php
-	class contactController extends MyController {
+	include_once PROTECTED_DIR.'subpages/estate_agency/core/validation.php';
+    class contactController extends MyController {
 	    public function __construct() {
 	        parent::__construct();
 	        $this->setModel('contactModel');
+            $this->validation = new Validation();
     	}
 
-    	private $name;
+    	private $validation;
+
+        private $name;
     	private $email;
     	private $subject;
     	private $addressedId;
@@ -16,14 +20,29 @@
     		return $agents;
     	}
 
-    	public function sendMessage() {
+    	public function getErrorMessage($name, $email, $message) {
+            $errorMessage = "";
+            if(!$this->validation->checkEmail($email)) {
+                $errorMessage = "Szabálytalan email cím! Minta: username@domain.com";
+            }
+
+            $regex = "/^[A-ZÁÉÍÓÖŐÚÜŰ][a-záéííóöőúü]{1,19}(?:[ -][A-ZÁÉÍÓÖŐÚÜŰ][a-záéííóöőúü]{1,19}){0,2}$/";
+            if(!$this->validation->checkStringByRegex($name, $regex)) {
+                $errorMessage = "Nem megfelelő nevet adott meg!";
+            }
+            if(!$this->validation->checkStringLength(5, 500, $message)) {
+                $errorMessage = "Az üzenet 5 és 500 karakter közöttinek kell lennie!";
+            }
+        }
+
+        public function sendMessage() {
     		if(array_key_exists('send', $_POST)) {
     			$this->name = $_POST['name'];
     			$this->email = $_POST['email'];
     			$this->subject = $_POST['subject'];
     			$this->addressedId = $_POST['addressed'];
     			$this->message = $_POST['message'];
-    			$errorMessage = "";
+    			$errorMessage = $this->getErrorMessage($this->name, $this->email, $this->message);
     			if($errorMessage == "") {
     				$message = $this->model->sendMessage($this->name, $this->email, $this->subject, $this->addressedId, $this->message);
     				$selectedAgent = $this->model->getAgentById($this->addressedId);
