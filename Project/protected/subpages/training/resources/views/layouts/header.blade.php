4b19dacd17a0aed5d81ecb500e44fff5f27cf7e9
Spokieh <dohanyos96@gmail.com>
2021-04-10 19:19:03 +0200

    Training: rendelesek oldal stilusigazitasa

--- Project/protected/subpages/training/resources/views/layouts/header.blade.php
+++ Project/protected/subpages/training/resources/views/layouts/header.blade.php
@@ -22,7 +22,7 @@ if(Session::has('user'))
 		<div class="navbar-nav ml-auto">
 			<a href="/" class="nav-item nav-link active"><i class="fa fa-home"></i><span>Főoldal</span></a>
       @if(Session::has('user'))
-			<a href="/myorders" class="nav-item nav-link"><i class="fa fa-calendar-check-o"></i><span>Rendelés</span></a>
+			<a href="/myorders" class="nav-item nav-link"><i class="fa fa-calendar-check-o"></i><span>Rendeléseid</span></a>
 			<a href="/cart" class="nav-item nav-link"><i class="fa fa-shopping-cart"></i><span>Kosár ({{ $total }})</span></a>
       <div class="nav-item dropdown ">
 				<a href="#" data-toggle="dropdown" class="nav-item nav-link dropdown-toggle user-action"><i class="fa fa-user" aria-hidden="true"></i> {{ Session::get('user')['name'] }}<b class="caret"></b></a>
--- Project/protected/subpages/training/resources/views/myorders.blade.php
+++ Project/protected/subpages/training/resources/views/myorders.blade.php
@@ -1,29 +1,37 @@
 @extends('layouts.master')
 @section("content")
 <div class="custom-product">
-     <div class="col-sm-10">
+     <div class="col-sm-12">
         <div class="trending-wrapper">
-            <h4>Rendelesem</h4>
-            @foreach($orders as $item)
+          <div class="ordered-title">
+            <h3>Rendeléseid</h3>
             <div class=" row searched-item cart-list-devider">
-             <div class="col-sm-3">
-                <a href="detail/{{$item->id}}">
+            </div>
+          </div>
+            <div>
+              @foreach($orders as $item)
+              <div class=" row searched-item cart-list-devider">
+                <div class="col-sm-3 ordered-item-img">
+                  <a href="detail/{{$item->id}}">
                     <img class="trending-image" src="{{$item->gallery}}">
                   </a>
-             </div>
-             <div class="col-sm-4">
-                    <div class="">
-                      <h2>Nev: {{$item->name}}</h2>
-                      <h5>Szallitasi statusz: {{$item->status}}</h5>
-                      <h5>Cim: {{$item->address}}</h5>
-                      <h5>Fizetesi statusz: {{$item->payment_status}}</h5>
-                      <h5>Fizetesi mod: {{$item->payment_method}}</h5>
-                    </div>
-             </div>
+              </div>
+              <div class="col-sm-9">
+                <div class="">
+                  <h2>Termék: {{$item->name}}</h2>
+                  <div class="ordered-list-details">
+                    <u><h4><strong>Rendelesi informaciok:</strong></h4></u>
+                    <h5><strong>Szállítási státusz:</strong> {{$item->status}}</h5>
+                    <h5><strong>Cím:</strong> {{$item->address}}</h5>
+                    <h5><strong>Fizetési státusz:</strong> {{$item->payment_status}}</h5>
+                    <h5><strong>Fizetési mód:</strong> {{$item->payment_method}}</h5>
+                  </div>
+                  </div>
+              </div>
             </div>
             @endforeach
           </div>
-
+          </div>
      </div>
 </div>
 @endsection 
