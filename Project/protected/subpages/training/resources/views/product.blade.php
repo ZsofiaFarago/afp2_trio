99af60d5186db6d50fb22c099dfcde6de0edf81d
Spokieh <dohanyos96@gmail.com>
2021-04-10 17:30:11 +0200

    Training: kep slider

--- Project/protected/subpages/training/resources/views/product.blade.php
+++ Project/protected/subpages/training/resources/views/product.blade.php
@@ -1,49 +1,59 @@
 @extends('layouts.master')
 @section('content')
+
 <div class="container custom-product" >
 
-      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+<div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class="active" aria-current="true"></button>
+    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class=""></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item">
+      <div class="overlay-image" style="background-image: url(https://shopbuilder.hu/images/bbcomment/hu_202104_sc_vitaking_monthly_offers_1200x628.jpg); background-size: 80%; background-repeat: no-repeat;"></div>
+      <div class="container">
+        <div class="carousel-caption text-start">
         </div>
-        <div class="carousel-inner">
-            @foreach ($products as $product)
-            <div class="item {{ $product['id']==1 ? 'active' : '' }}">
-               <a href="detail/{{ $product['id'] }}">
-                <img class="slider-img" src="{{ $product['gallery'] }}">
-                <div class="carousel-caption slider-text">
-                  <h3>{{ $product['name'] }}</h3>
-                  <p>{{ $product['description'] }}</p>
-                </div>
-            </a>
-            </div>
-            @endforeach
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
       </div>
-      <div class="trending-wrapper">
-        <h3>Felkapott termekek</h3>
-        
-        @foreach ($products as $product)
-        <div class="trending-item">
-          <a href="detail/{{ $product['id'] }}">
-            <img class="trending-image" src="{{ $product['gallery'] }}">
-            <div class="">
-              <h3>{{ $product['name'] }}</h3>
-            </div>
-          </a>
+    </div>
+    <div class="carousel-item active">
+      <div class="overlay-image" style="background-image: url(https://shopbuilder.hu/images/bbcomment/hu_202104_sc_usa_monthly_offers_1200x628.jpg); background-size: 80%; background-repeat: no-repeat;"></div>
+      <div class="container">
+        <div class="carousel-caption">    
         </div>
-            @endforeach
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="overlay-image" style="background-image: url(https://shopbuilder.hu/images/bbcomment/hu_202104_sc_proteinbuzz_monthly_offers_1200x628.jpg); background-size: 80%; background-repeat: no-repeat;"></div>
+      <div class="container">
+        <div class="carousel-caption text-end">
+        </div>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
-      </div>
+  <div class="trending-wrapper">
+    <h3>Felkapott termekek</h3>
+    @foreach ($products as $product)
+    <div class="trending-item">
+      <a href="detail/{{ $product['id'] }}">
+        <img class="trending-image" src="{{ $product['gallery'] }}">
+        <div class="">
+          <h3>{{ $product['name'] }}</h3>
+        </div>
+      </a>
+    </div>
+    @endforeach
+  </div>
 </div>
 @endsection
