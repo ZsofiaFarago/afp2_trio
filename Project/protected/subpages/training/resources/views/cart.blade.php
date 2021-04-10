d805c12cbb55800703007ce4b49ecf5cf5402947
Spokieh <dohanyos96@gmail.com>
2021-04-10 18:02:13 +0200

    Training: kosar stilus igazitas

--- Project/protected/subpages/training/resources/views/cart.blade.php
+++ Project/protected/subpages/training/resources/views/cart.blade.php
@@ -1,31 +1,55 @@
 @extends('layouts.master')
 @section("content")
-<div class="custom-product">
-     <div class="col-sm-10">
-        <div class="trending-wrapper">
-            <h4>Kosar tartalma</h4>
-            <a class="btn btn-success" href="ordernow">Penztar</a> <br> <br>
-            @foreach($products as $item)
-            <div class=" row searched-item cart-list-devider">
-             <div class="col-sm-3">
-                <a href="detail/{{$item->id}}">
-                    <img class="trending-image" src="{{$item->gallery}}">
+<div class="custom-product" style="padding-top: 5%; padding-left: 20%;">
+<div class="col-sm-12 col-md-10 col-md-offset-1">
+  <table class="table table-hover">
+  <thead>
+  <tr>
+  <th><h2>Termékek</h2></th>
+  <th class="text-center">Ár</th>
+  <th> </th>
+  </tr>
+  </thead>
+  <tbody>
+    @foreach ($products as $item)  
+    <tr>
+        <td class="col-sm-8 col-md-6">
+            <div class="media">
+                <a class="thumbnail pull-left" href="detail/{{$item->id}}"> <img class="media-object" src="{{$item->gallery}}" style="width: 72px; height: 72px;"> </a>
+                <div class="media-body">
+                    <h4 class="media-heading"><a href="#">{{$item->name}}</a></h4>
+                  </div>
+              </div></td>
+              <td class="col-sm-1 col-md-1 text-center"><strong>{{ $item->price }}</strong></td>
+              <td class="col-sm-1 col-md-1">
+                  <a href="/removecart/{{$item->cart_id}}">
+                      <button type="button" class="btn btn-danger">
+                          <span class="fa fa-remove"></span> Eltávolítás
+                      </button></td>
                   </a>
-             </div>
-             <div class="col-sm-4">
-                    <div class="">
-                      <h2>{{$item->name}}</h2>
-                      <h5>{{$item->description}}</h5>
-                    </div>
-             </div>
-             <div class="col-sm-3">
-                <a href="/removecart/{{$item->cart_id}}" class="btn btn-warning" >Eltavolitas kosrbol</a>
-             </div>
-            </div>
-            @endforeach
-          </div>
-          <a class="btn btn-success" href="ordernow">Vasarlas Azonnal</a> <br> <br>
-
-     </div>
+              </tr>
+      @endforeach
+  <tr>
+  <td>   </td>
+  <td>
+    <a href="/">
+      <button type="button" class="btn btn-default">
+        <span class="fa fa-shopping-cart"></span> 
+        Vásárlás folytatása
+      </button>
+    </a>
+    </td>
+  <td>
+    <a href="ordernow">
+      <button type="button" class="btn btn-success">
+        Pénztár 
+        <span class="fa fa-play"></span>
+      </button>
+    </a>
+    </td>
+    </tr>
+  </tbody>
+  </table>
+  </div>
 </div>
 @endsection 
