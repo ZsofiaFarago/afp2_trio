a813540d105bb73225d6d74cd7b380587639b9cc
ZsofiaFarago <farago.zsofia.1995@gmail.com>
2021-04-13 00:38:08 +0200

    Ingatlanos o.: hibaüzenetlista a formokhoz 1 hiba helyett

--- Project/protected/subpages/estate_agency/view/formErrorMessageView.php
+++ Project/protected/subpages/estate_agency/view/formErrorMessageView.php
@@ -1,3 +1,9 @@
-<?php
-	if(isset($errorMessage)) { echo '<div class = "formResult formErrorMessage">'.$errorMessage.'</div>'; }
-?>
+<?php if(isset($errorMessages)): ?>
+	<div class = "formResult formErrorMessage">
+		<ul>
+			<?php foreach($errorMessages as $errorMessage): ?>
+				<li><?=$errorMessage?></li>
+			<?php endforeach; ?>
+		</ul>
+	</div>
+<?php endif; ?>
