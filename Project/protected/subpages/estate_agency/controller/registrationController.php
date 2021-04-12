cbd338eb61b2ae348d3abeb2ad0fb72ff5d0f882
ZsofiaFarago <farago.zsofia.1995@gmail.com>
2021-04-13 00:36:30 +0200

    Ingatlanos o.: hibakezelés átalakítva a regisztrációs controllerben

--- Project/protected/subpages/estate_agency/controller/registrationController.php
+++ Project/protected/subpages/estate_agency/controller/registrationController.php
@@ -1,13 +1,13 @@
 <?php
-    include_once PROTECTED_DIR.'subpages/estate_agency/core/validation.php';
+    include_once PROTECTED_DIR.'subpages/estate_agency/core/errorMessageMaker.php';
 	class registrationController extends MyController {
 	    public function __construct() {
 	        parent::__construct();
 	        $this->setModel('registrationModel');
-            $this->validation = new Validation();
+            $this->errorMessageMaker = new ErrorMessageMaker();
     	}
 
-        private $validation;
+        private $errorMessageMaker;
 
         private $firstName;
         private $lastName;
@@ -38,85 +38,50 @@
 	    	return $addressRecord['id'];
 	    }
 
-    	private function getErrorMessage() {
-            $errorMessage = "";
+    	private function getErrorMessages() {
+            $this->errorMessageMaker->setFirstNameErrorMessage($this->firstName);
+            $this->errorMessageMaker->setLastNameErrorMessage($this->lastName);
+            $this->errorMessageMaker->setEmailErrorMessage($this->email);
+            $this->errorMessageMaker->setPhoneErrorMessage($this->phone);
+            $this->errorMessageMaker->setZipcodeErrorMessage($this->zipcode);
+            $this->errorMessageMaker->setCityErrorMessage($this->city);
+            $this->errorMessageMaker->setAddressErrorMessage($this->streetName);
+            $this->errorMessageMaker->setStreetNumberErrorMessage($this->streetNumber);
+            $this->errorMessageMaker->setPasswordErrorMessage($this->password, $this->passwordAgain);
 
-            $regex = "/^[A-ZÁÉÍÓÖŐÚÜŰ][a-záéííóöőúü]{1,19}(?:[ -][A-ZÁÉÍÓÖŐÚÜŰ][a-záéííóöőúü]{1,19}){0,2}$/";
-            if (!$this->validation->checkStringByRegex($this->lastName, $regex)) {
-                $errorMessage = "Hibásan adta meg a vezetéknevét! A neve minden tagjában az első karakter nagy, a többi kicsi legyen! Nem tartalmazhat számjegyeket, speciális karakterket vagy felesleges szóközöket. Ha több tagból áll a neve, a tagokat egy szóköz vagy kötőjel választhatja el.";
-            }
-
-            if(!$this->validation->checkStringByRegex($this->firstName)) {
-                $errorMessage = "Hibásan adta meg a keresztnevét! A neve minden tagjában az első karakter nagy, a többi kicsi legyen! Nem tartalmazhat számjegyeket, speciális karakterket vagy felesleges szóközöket. Ha több tagból áll a neve, a tagokat egy szóköz választhatja el.";
-            }
-
-            if(!$this->validation->checkEmail($this->firstName)) {
-                $errorMessage = "Hibás email cím! Minta: username@domain.com";
-            }
-
-            $regex = "/^(06)[ -]{0,1}(20|30|70|90)[ -]{0,1}[0-9]{3}[ -]{0,1}[0-9]{4}$/";
-            if(!$this->validation->checkStringByRegex($this->phone, $regex)) {
-                $errorMessage = "A telefonszámot helytelenül adta meg! Mobilszámot kell megadnia, 06-tal kell kezdődenie, a következő 2 számjegy a hívószám (20, 30, 70 vagy 90), utána a telefonszám 7 karaktere következik. Minták: 
-                        <ul>
-                            <li>06201234567</li>
-                            <li>06 30 123 4567</li>
-                            <li>06-70-123-4567</li>
-                            <li>06 90 123-4567</li>
-                        </ul>";
-            }
-
-            $regex = "/^[A-ZÁÉÍÓÖŐÚÜŰ][a-záéííóöőúü]{1,19}(?:[ -][A-ZÁÉÍÓÖŐÚÜŰa-záéííóöőúü][a-záéííóöőúü]{1,19}){0,2}$/";
-            if(!$this->validation->checkStringByRegex($this->city, $regex)) {
-                $errorMessage = "A település nevét helytelenül adta meg!";
-            }
-
-            $regex = "/^[A-ZÁÉÍÓÖŐÚÜŰ][a-záéííóöőúü]{1,19}((?:[ -][A-ZÁÉÍÓÖŐÚÜŰ][a-záéííóöőúü]{1,19}){0,2}( )(utca|u.|út|tér|köz|fasor|körút|sétány))$/";
-            if(!$this->validation->checkStringByRegex($this->streetName, $regex)) {
-                $errorMessage = "Az utcanév helytelen. Az utcanév nagy kezdőbetűvel kezdődik, nem tartalmaz számokat, spec. karaktereket, tagjait szóközzel vagy kötőjellel lehet elválasztani, a végén pedig szóközzel elválasztva ki kell tenni a közterület típusát, ami lehet: út, utca, u., tér, körút, köz, fasor, sétány.";
-            }
-
-            $regex = "/^[1-9][0-9]{0,2}([\/][A-Z]{1}){0,1}$/";
-            if(!$this->validation->checkStringByRegex($this->streetNumber, $regex)) {
-                $errorMessage = "A házszámot helytelenül adta meg, helyes formátumok: 8, 12, 11/A, 23/B stb.";
-            }
-
-            $regex = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/";
-            if(!$this->validation->checkStringByRegex($this->password, $regex)) {
-                $errorMessage = "Nem megfelelő jelszót választott! A jelszónak legalább 8 karakteresnek kell lennie, tartalmaznia kell legalább egy kisbetűt, legalább egy nagybetűt és legalább egy számjegyet!";
-            }
-
-            if($this->password != $this->passwordAgain) {
-                $errorMessage = "A két jelszó nem egyezik meg!";
-            }
+            $errorMessages = $this->errorMessageMaker->getErrorMessages();
 
             if($this->model->getClientByEmail($this->email) != null) {
                 $errorMessage = "A megadott email címmel már regisztráltak az oldalra!";
+                array_push($errorMessages, $errorMessage);
             }
+            return $errorMessages;
+        }
 
-            return $errorMessage;
+        private function getPostData() {
+            $this->firstName = $_POST['firstName'];
+            $this->lastName = $_POST['lastName'];
+            $this->email = $_POST['email'];
+            $this->phone = $_POST['phone'];
+            $this->city = $_POST['city'];
+            $this->zipcode = $_POST['zipcode'];
+            $this->streetName = $_POST['streetName'];
+            $this->streetNumber = $_POST['streetNumber'];
+            $this->password = $_POST['password'];
+            $this->passwordAgain = $_POST['passwordAgain'];
         }
 
         public function registerNewClient() {
     		if(array_key_exists("save", $_POST)) {
-    			$this->firstName = $_POST['firstName'];
-    			$this->lastName = $_POST['lastName'];
-    			$this->email = $_POST['email'];
-    			$this->phone = $_POST['phone'];
-    			$this->city = $_POST['city'];
-    			$this->zipcode = $_POST['zipcode'];
-    			$this->streetName = $_POST['streetName'];
-    			$this->streetNumber = $_POST['streetNumber'];
-    			$this->password = $_POST['password'];
-    			$this->passwordAgain = $_POST['passwordAgain'];
-
-    			$errorMessage = $this->getErrorMessage();
-                if($errorMessage == "") {
+                $this->getPostData();
+    			$errorMessages = $this->getErrorMessages();
+                if(count($errorMessages) == 0) {
     				$cityId = $this->saveCityData($this->city, $this->zipcode);
     				$addressId = $this->saveAddressData($this->streetName, $this->streetNumber, $cityId);
     				$this->model->registerClient($this->firstName, $this->lastName, $this->email, $this->phone, $this->password, $addressId);
     				$this->renderPage('successfulRegistrationView');
     			} else {
-    				$this->addViewParams('errorMessage', $errorMessage);
+    				$this->addViewParams('errorMessages', $errorMessages);
                 	$this->renderPage('formErrorMessageView');
     			}
                 unset($_POST);
