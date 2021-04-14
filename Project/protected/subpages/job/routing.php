a2945280c5769e9debc971216d2a1a8597d8692b
Ravasz Bálint <ravasz.balint7@gmail.com>
2021-04-14 15:12:35 +0200

    Állásportál oldal: Casek felülvizsgálata

--- Project/protected/subpages/job/routing.php
+++ Project/protected/subpages/job/routing.php
@@ -1,11 +1,16 @@
 <?php 
-	if(!array_key_exists('A', $_GET) || empty($_GET['A']))
-		$_GET['A'] = 'home';
-	switch ($_GET['A']) {
-		case 'home': require_once PROTECTED_DIR.'subpages/job/main.php'; break;
-		case 'register': require_once PROTECTED_DIR.'subpages/job/register.php'; break;
-		case 'menu2': require_once PROTECTED_DIR.'subpages/job/menu2.php'; break;
-
-		default: require_once PROTECTED_DIR.'error/404.php'; break;
+	if(!array_key_exists('P', $_GET) || empty($_GET['P'])){
+		include_once 'main.php';
 	}
+		else {
+	switch ($_GET['P']) {
+		//case 'home': require_once PROTECTED_DIR.'subpages/job/main.php'; break;
+		case 'logout' : session_unset(); session_destroy(); header ('Location subpages/job/index.php'); break;
+		case 'register': require_once PROTECTED_DIR.'subpages/job/register.php'; break;
+		case 'create': require_once PROTECTED_DIR.'subpages/job/create.php'; break;
+		case 'find' : require_once PROTECTED_DIR.'subpages/job/find.php'; break;
+		case 'job' : require_once PROTECTED_DIR.'subpages/job/job.php' break;
+		default: include_once 'main.php'; break;
+	}
+}
 ?>
