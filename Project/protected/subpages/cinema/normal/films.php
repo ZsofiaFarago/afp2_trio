45b6b3baf771f067564f4246bea54f094ae5cc4c
Marci <pmarcell50@gmail.com>
2021-04-14 19:50:25 +0200

    Mozi oldal: filmek adatbázisból való lekérése

--- Project/protected/subpages/cinema/normal/films.php
+++ Project/protected/subpages/cinema/normal/films.php
@@ -2,19 +2,36 @@
 <hr>
 <p>FEJLESZTÉS ALATT...</p>
 
+<?php 
+	$query = "SELECT id, name, time, price FROM films";
+	require_once DATABASE_CONTROLLER;
+	$items = getList($query);
+
+ ?>
+
+
 <div class="item">
-	<table style="table-layout: fixed; width: 60%; border: 3px solid gray; margin-left: auto; margin-right: auto;   background-color: #bababa;">
-		<tr>
-			<td colspan="2"><center>Film neve</center></td>			
-		</tr>
-		<tr>
-			<td><b>Elérhető időpontok:</b></td>
-			<td style="text-align: center"></td>			
-		</tr>
-		<tr>
-			<td>Leírás..</td>
-		<tr>
-		<td colspan="2">Ár: ... <center>										
-			</tr>
-	</table>
+	<?php foreach ($items as $i) : ?>
+				<div class="item">
+						<table style="table-layout: fixed; width: 60%; border: 3px solid gray; margin-left: auto; margin-right: auto;   background-color: #bababa;">
+						<tr>
+							<td></td>			
+						</tr>
+						<tr>
+							<td><b>Név:</b> <?=$i['name'] ?></td>
+							<td style="text-align: center">
+								<?=$i['price'] ?><p>Ft</p>
+							</td>	
+						</tr>
+						<tr>
+							<td><b>Időpont: <?=$i['time'] ?></b></td>	
+						</tr>
+						<hr>
+						<tr>
+							
+						</tr>
+					</table>
+				</div>
+				<hr style="margin-bottom: 12px; margin-top: 12px;">
+		<?php endforeach; ?>
 </div>
