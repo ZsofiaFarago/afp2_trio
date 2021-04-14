7a6745c400afcb659d581403db5742f34244fc29
Marci <pmarcell50@gmail.com>
2021-04-14 22:21:38 +0200

    Mozi oldal: SQL fájl frissítés

--- Project/protected/subpages/cinema/sql/cinema.sql
+++ Project/protected/subpages/cinema/sql/cinema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2021. Már 10. 22:15
+-- Létrehozás ideje: 2021. Ápr 14. 22:20
 -- Kiszolgáló verziója: 10.4.11-MariaDB
 -- PHP verzió: 7.4.4
 
@@ -34,6 +34,14 @@ CREATE TABLE `films` (
   `price` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- A tábla adatainak kiíratása `films`
+--
+
+INSERT INTO `films` (`id`, `name`, `time`, `price`) VALUES
+(1, 'Csipkerózsika', '17:00', 1390),
+(2, 'Hamupipőke', '21:00', 1390);
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +53,28 @@ CREATE TABLE `seats` (
   `number` int(10) NOT NULL,
   `available` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(64) NOT NULL,
+  `fname` varchar(200) NOT NULL,
+  `lname` varchar(200) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `permission` int(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- A tábla adatainak kiíratása `users`
+--
+
+INSERT INTO `users` (`id`, `fname`, `lname`, `email`, `password`, `permission`) VALUES
+(1, 'Pásztor', 'Marcell', 'pmarcell50@gmail.com', '123456', 2);
 
 --
 -- Indexek a kiírt táblákhoz
@@ -63,6 +93,12 @@ ALTER TABLE `seats`
   ADD PRIMARY KEY (`id`);
 
 --
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- A kiírt táblák AUTO_INCREMENT értéke
 --
 
@@ -70,13 +106,19 @@ ALTER TABLE `seats`
 -- AUTO_INCREMENT a táblához `films`
 --
 ALTER TABLE `films`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT a táblához `seats`
 --
 ALTER TABLE `seats`
   MODIFY `id` int(2) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(64) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Megkötések a kiírt táblákhoz
