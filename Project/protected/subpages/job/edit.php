84b76ab6dcae6502089ba4b6464a5cd9a347af73
Ravasz Bálint <ravasz.balint7@gmail.com>
2021-04-14 14:44:18 +0200

    Állásportál oldal:Hirdetésmódosítás (edit) művelet

---
+++ Project/protected/subpages/job/edit.php
@@ -0,0 +1,18 @@
+<?php include_once 'config/init.php'; ?>
+
+<?php
+$job = new Job;
+
+$job_id = isset($_GET['id']) ? $_GET['id'] : null;
+
+if(isset($_POST['submit'])){
+	$data = array();
+	$data['job_title'] = $_POST['job_title'];
+	$data['company'] = $_POST['company'];
+	$data['category_id'] = $_POST['category'];
+	$data['description'] = $_POST['description'];
+	$data['location'] = $_POST['location'];
+	$data['salary'] = $_POST['salary'];
+	$data['contact_user'] = $_POST['contact_user'];
+	$data['contact_email'] = $_POST['contact_email'];
+
