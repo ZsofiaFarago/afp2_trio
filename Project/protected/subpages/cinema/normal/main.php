3010a35c34d2806b8cb6b5a28540eafe03fb131e
Marci <pmarcell50@gmail.com>
2021-04-14 20:21:58 +0200

    Mozi oldal: bejelentkezés működőképessége, bejelentkezés utáni üzenet

--- Project/protected/subpages/cinema/normal/main.php
+++ Project/protected/subpages/cinema/normal/main.php
@@ -1,2 +1,7 @@
+<?php if(isset($_GET['loggedIn'])) : ?>
+	<h3>Sikeres bejelentkezés!</h3>
+	<hr>
+	<h4>Jogosultsági szint: <?=$_SESSION['permission']; ?></h4>
+<?php endif; ?>
 Üdvözöljük az AFP Mozi oldalán.
 Böngészéshez kattintson a Műsorlista menüpontra.
