720a625a912138a8af89ca2a02fdc8d6c44a14b4
Spokieh <dohanyos96@gmail.com>
2021-04-14 21:04:58 +0200

    Training: penzter stilusa formalva

--- Project/protected/subpages/training/resources/views/ordernow.blade.php
+++ Project/protected/subpages/training/resources/views/ordernow.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.master')
 @section("content")
 <div class="wrapper">
-  <img class="ironhack-img" src="https://course_report_production.s3.amazonaws.com/rich/rich_files/rich_files/827/s200/ironhacklogo-compressor-20-1-.png" alt="">
+
   <div class="text-header">
     <h1>AddedMuscle<small> Kosarad tartalma</small></h1>
-    <p>Here you your order resume. All the products listed, with the detail about the quantity, unit price, and total price. If you got any further question just call +34 132 31 32 32 or contact to problems@ironshop.com</p>
   </div>
 
   <table class="table table-striped">
@@ -24,7 +23,7 @@
       <td>{{ $item->price }} Ft</td>
     </tr>
     @endforeach
-      <th scope="row">Szallitasi dij</th>
+      <th scope="row">Szállítási díj</th>
       <th> 1290 Ft</th>
     </tr>
     <tr class="table-warning">
@@ -32,54 +31,30 @@
       <td>{{ $total+1290 }} Ft</td>
     </tr>
   </tbody>
-</table>
-<div>
-  Szállítás és fizetés
-</div>
-<div class="button-wrapper">
-  <button type="button" class="btn btn-success btn-checkout">Fizetés</button>
-  <button type="button" class="btn btn-light btn-continous">Vásárlás folytatása</button>
-</div>
-<small class="order-info">Megrendelését a következő 48 órán belül kiszállítjuk.</small>
-</div
-
-
-
-
-{{-- <div class="custom-product">
-  <div class="col-sm-10">
-     
-     <table class="table">
-         <tbody>
-           <tr>
-             <td>Termkekek osszesen</td>
-             <td>{{ $total }} Ft</td>
-           </tr>
-           <tr>
-             <td>Szallitasi dij</td>
-             <td>1290 Ft</td>
-           </tr>
-           <tr>
-             <td>Összesen</td>
-             <td>{{ $total+1290 }} Ft</td>
-           </tr>
-         </tbody>
-       </table>
-
-       <form action="/orderplace" method="POST">
-         @csrf
-         <div class="mb-3">
-           <textarea name="address" class="form-control" placeholder="Szallitasi cim"></textarea>
-         </div>
-         <div class="mb-3">
-           <label for="">Fizetesi mod</label> <br> <br>
-           <input type="radio" name="payment" value="cash"><span>Online bankkartyas fizetes</span> <br> <br>
-           <input type="radio" name="payment" value="cash"><span>Banki utalas</span> <br> <br>
-           <input type="radio" name="payment" value="cash"><span>Utanvettel</span> <br> <br>
-         </div>
-         <button type="submit" class="btn btn-primary">Vasarlas</button>
-       </form>
-
+  </table>
+  <div class="pt-3">
+    <h1>Szállítás és fizetés</h1>
   </div>
-</div> --}}
+  <form action="/orderplace" method="POST">
+    @csrf
+    <div class="mb-3">
+      <div>
+        <input type="radio" name="payment" value="cash" id="payment" style="padding-right: 10% !important;">
+        <span class="mr-4">Online bankkártyás fizetés</span>
+        <input type="radio" name="payment" value="cash" id="payment"><span>Banki utalás</span> <br> <br>
+        <input type="radio" name="payment" value="cash" id="payment"><span>Utánvéttel</span> <br> <br>
+      </div>
+    </div>
+
+    <div>
+      <p>Szállítási cím:</p>
+      <textarea name="address" class="form-control" placeholder="Szállítási cím (Irsz, Város, Út, Házszám)"></textarea>
+    </div>
+
+    <div class="button-wrapper pb-4">
+      <button type="submit" class="btn btn-success btn-checkout">Fizetés</button>
+      <button type="button" class="btn btn-light btn-continous">Vásárlás folytatása</button>
+    </div>
+  </form>
+</div>
 @endsection 
