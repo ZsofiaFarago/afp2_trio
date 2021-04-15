7924074e8d6ec2f4f4a3f44f95a2f9a1ecd28a95
Spokieh <dohanyos96@gmail.com>
2021-04-15 13:11:37 +0200

    Training - termekek egyedi oldalanak stilusigazaitasa

--- Project/protected/subpages/training/resources/views/detail.blade.php
+++ Project/protected/subpages/training/resources/views/detail.blade.php
@@ -1,24 +1,21 @@
 @extends('layouts.master')
 @section('content')
-<div class="container">
+<div class="detail-page">
     <div class="row">
         <div class="col-sm-6">
-            <img class="detail-img" src="{{ $product['gallery'] }}" alt="">
+            <img class="detail-img mx-auto d-block " src="{{ $product['gallery'] }}" alt="">
         </div>
         <div class="col-sm-6">
-            <a href="/">Vissza</a>
-            <h2>{{ $product['name'] }}</h2>
-            <h3>Ar: {{ $product['price'] }}</h3>
-            <h4>Leiras: {{ $product['description'] }}</h4>
+            <a href="/products">Vissza</a>
+            <h2 style="padding-top: 10px">{{ $product['name'] }}</h2>
+            <h3 style="padding-top: 10px">Ar: {{ $product['price'] }} Ft</h3>
+            <h4 style="padding-top: 10px">Leiras: {{ $product['description'] }}</h4>
             <br><br>
             <form action="/add_to_cart" method="POST">
                 @csrf
-                <input type="hide" name="product_id" value="{{ $product['id'] }}">
+                <input type="hidden" name="product_id" value="{{ $product['id'] }}">
                 <button class="btn btn-primary">Kosarba</button>
             </form>
-            <br><br>
-            <button class="btn btn-success">Vasarlas Azonnal</button>
-            <br><br>
         </div>
     </div>
 
