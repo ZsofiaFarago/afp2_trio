0a36eb0004a0b24b55dcf9d7c7da26a413681d07
Spokieh <dohanyos96@gmail.com>
2021-04-15 22:25:50 +0200

    Training - fooldalrol atiranyitas

---
+++ Project/protected/subpages/training/start.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<meta content="charset=UTF-8">
+	<title>Training oldal</title>
+	<meta http-equiv="refresh" content="0; url = http://localhost:8000/added_muscle" />
+</head>
+
+<body>
+</body>
+
+</html>
