31769ee3c82c98a165cb8b9c78dfb10edef8446c
ZsofiaFarago <farago.zsofia.1995@gmail.com>
2021-04-17 15:55:12 +0200

    Ingatlan o.: reverse tábla hozzáadva a db-hez, estate tábla módisítva

--- Project/protected/subpages/estate_agency/sql/estatedb.sql
+++ Project/protected/subpages/estate_agency/sql/estatedb.sql
@@ -54,10 +54,29 @@ CREATE TABLE IF NOT EXISTS estatedb.estate (
   room_number INT NOT NULL,
   description VARCHAR(500) NOT NULL,
   image_name VARCHAR(50) NOT NULL,
+  client_id INT,
   PRIMARY KEY (id),
   CONSTRAINT fk_estate_address
     FOREIGN KEY (address_id)
-    REFERENCES estatedb.address (id));
+    REFERENCES estatedb.address (id),
+  CONSTRAINT fk_estate_client
+    FOREIGN KEY (client_id)
+    REFERENCES estatedb.client (id));
+
+-- -----------------------------------------------------
+-- Table estatedb.reserve
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS estatedb.reserve (
+  id INT NOT NULL AUTO_INCREMENT,
+  estate_id INT NOT NULL,
+  client_id INT NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_reserve_estate
+    FOREIGN KEY (estate_id)
+    REFERENCES estatedb.estate (id),
+  CONSTRAINT fk_reserve_client
+    FOREIGN KEY (client_id)
+    REFERENCES estatedb.client (id));
 
 -- -----------------------------------------------------
 -- Table estatedb.estate_agent
@@ -120,7 +139,12 @@ INSERT INTO `estatedb`.`estate_agent` (`id`, `first_name`, `last_name`, `email`,
 INSERT INTO `estatedb`.`estate_agent` (`id`, `first_name`, `last_name`, `email`, `phone`, `address_id`, `speciality`, `image_name`) VALUES ('3', 'Anakin', 'Skywalker', 'anakin.s@estate.com', '06 90 555 3930', '1', 'Energetika, klíma, fűtés', 'agent_003.png');
 INSERT INTO `estatedb`.`estate_agent` (`id`, `first_name`, `last_name`, `email`, `phone`, `address_id`, `speciality`, `image_name`) VALUES ('4', 'Perselus', 'Piton', 'perselus.piton@estate.com', '06 90 555 2456', '3', 'Teljes szakterület', 'agent_004.jpg');
 
-INSERT INTO `estatedb`.`estate` (`id`, `address_id`, `selling_price`, `purchase_price`, `area`, `room_number`, `description`, `image_name`) VALUES ('1', '6', '500000000', '395000000', '110', '10', 'Régi építésű ház, elszigetelt helyen található. Haszonállat tartására laklamas udvar tartozik hozzá. Van terasz is.', 'estate_002.jpg');
-INSERT INTO `estatedb`.`estate` (`id`, `address_id`, `selling_price`, `purchase_price`, `area`, `room_number`, `description`, `image_name`) VALUES ('2', '7', '1000000000', '900000000', '250', '15', 'Nagy ház, sok szobával, légkondícionálóval, erdős területre néző kilátással, graázzsal, medencével. Felújításra szorul a konyha és a fürdőszobák.', 'estate_003.jpg');
-INSERT INTO `estatedb`.`estate` (`id`, `address_id`, `selling_price`, `purchase_price`, `area`, `room_number`, `description`, `image_name`) VALUES ('3', '8', '80000000', '65000000', '60', '3', 'Két fürdőszobával és nagy konyhával rendelkező ház. Az árból lejön a rossz szomszédság és a felújításra szoruló fűtésrendszer.', 'estate_004.jpg');
-INSERT INTO `estatedb`.`estate` (`id`, `address_id`, `selling_price`, `purchase_price`, `area`, `room_number`, `description`, `image_name`) VALUES ('4', '9', '94000000', '88000000', '96', '4', 'Padlással és pincével rendelkező ház, hátsó bejárattal, veteményeskerttel. Kétszintes, a második szint könnyen leválasztható és külön lakrésszé alakítható.', 'estate_005.jpg');
+INSERT INTO `estate` (`id`, `address_id`, `selling_price`, `purchase_price`, `area`, `room_number`, `description`, `image_name`, `client_id`) VALUES ('1', '6', '500000000', '395000000', '110', '10', 'Régi építésű ház, elszigetelt helyen található. Haszonállat tartására laklamas udvar tartozik hozzá. Van terasz is.', 'estate_001.jpg', 1);
+INSERT INTO `estate` (`id`, `address_id`, `selling_price`, `purchase_price`, `area`, `room_number`, `description`, `image_name`, `client_id`) VALUES ('2', '7', '1000000000', '900000000', '250', '15', 'Nagy ház, sok szobával, légkondícionálóval, erdős területre néző kilátással, graázzsal, medencével. Felújításra szorul a konyha és a fürdőszobák.', 'estate_002.jpg', 2);
+INSERT INTO `estate` (`id`, `address_id`, `selling_price`, `purchase_price`, `area`, `room_number`, `description`, `image_name`, `client_id`) VALUES ('3', '8', '80000000', '65000000', '60', '3', 'Két fürdőszobával és nagy konyhával rendelkező ház. Az árból lejön a rossz szomszédság és a felújításra szoruló fűtésrendszer.', 'estate_003.jpg', 3);
+INSERT INTO `estate` (`id`, `address_id`, `selling_price`, `purchase_price`, `area`, `room_number`, `description`, `image_name`, `client_id`) VALUES ('4', '9', '94000000', '88000000', '96', '4', 'Padlással és pincével rendelkező ház, hátsó bejárattal, veteményeskerttel. Kétszintes, a második szint könnyen leválasztható és külön lakrésszé alakítható.', 'estate_004.jpg', 1);
+
+INSERT INTO `reserve` values(1, 1, 3);
+INSERT INTO `reserve` values(2, 2, 2);
+INSERT INTO `reserve` values(3, 3, 1);
+INSERT INTO `reserve` values(4, 4, 1);
