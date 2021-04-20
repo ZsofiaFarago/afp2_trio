ecab8a2f7435dfbc5a719af3cda8d1225a35ac32
Spokieh <dohanyos96@gmail.com>
2021-04-20 09:19:32 +0200

    Training - Chatbot css es js importalasa

--- Project/protected/subpages/training/resources/views/layouts/master.blade.php
+++ Project/protected/subpages/training/resources/views/layouts/master.blade.php
@@ -25,7 +25,9 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 
     {{-- CHATBOT --}}
+    <link rel="stylesheet" href="/css/jquery.convform.css">
     <script type="text/javascript" src="js/custom.js"></script>
+    <script type="text/javascript" src="js/jquery.convform.js"></script>
 </head>
 <body>
     {{ view('layouts.header') }}
